af4e6dcccea17ae21bf27085502c4361ce53fe95
kakasun <hejiademsn@live.cn>
2017-07-04 16:43:55 +0800

    检查传入的字符串中是否包含敏感词 添加注释

--- src/main/java/com/china/ciic/se/sejar/CreateSensitiveWordsLib.java
+++ src/main/java/com/china/ciic/se/sejar/CreateSensitiveWordsLib.java
@@ -1,18 +1,21 @@
 package com.china.ciic.se.sejar;
 
+import javax.annotation.PostConstruct;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 /**
- * 内容敏感词检查
  * Created by kakasun on 2017/7/4.
  */
 public class CreateSensitiveWordsLib {
-
+    /**
+     * 敏感词库
+     */
     static Map<Character,Node> SensitiveWordsLib = new HashMap<Character,Node>();
 
+    @PostConstruct
     public void init(){
         updateSensitiveWordsLib();
     }
@@ -22,6 +25,7 @@ public class CreateSensitiveWordsLib {
      */
     public void updateSensitiveWordsLib(){
         Map<Character,Node> lib = new HashMap<Character,Node>();
+        //敏感词应从数据库获取
         List<String> words = Arrays.asList("2b","你大爷的","怂","他妈的","屌","我他妈","你他妈");
         for (String s:words){
             add(s,lib);
@@ -107,9 +111,21 @@ public class CreateSensitiveWordsLib {
      * 敏感词节点
      */
     class Node{
+        /**
+         * 当前字符
+         */
         char currenChar;
+        /**
+         * 是否是铭感词的结束字符
+         */
         boolean end = false;
+        /**
+         * 是否有子节点
+         */
         boolean hasChildren = false;
+        /**
+         * 该节点的分支子节点
+         */
         Map<Character,Node> children;
 
         public char getCurrenChar() {
@@ -151,7 +167,7 @@ public class CreateSensitiveWordsLib {
                 " 我他妈抽一根烟。 你他妈让我靠边。 我他妈喝一口酒。 你他妈扭头就走。 我他妈牵你的手。 你他妈浑身发抖。" +
                 " 我他妈跟你接吻。 你他妈死活不肯。 你他妈无情无意。 我他妈决定放弃。 我他妈刚要跳楼。 你他妈才回过头。 " +
                 "你他妈回心转意。 我他妈刚好落地。 你他妈哭得像鬼 . 我他妈死的后悔 . 大海啊，他妈尽是水. 蜈蚣啊，他妈尽是腿 . " +
-                "辣椒啊，尽他妈辣嘴 . 有你们我他妈从不后悔.";
+                "辣椒啊，尽他妈辣嘴 . 有你们我他妈从不后悔.你个2b，你他妈真怂。你大爷的。我去你大爷的。";
         String w;
         for (int i = 0;i < testStr.length();i++){
             w = lib.checkOutSensitiveWord(i,testStr);
